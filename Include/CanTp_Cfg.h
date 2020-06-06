21000a729179b99464216b62c8250d9fa96297b4
Nada8773 <nada2478@gmail.com>
2020-06-06 21:57:12 +0200

    Update CanTp_Cfg.h

--- Include/CanTp_Cfg.h
+++ Include/CanTp_Cfg.h
@@ -30,18 +30,19 @@
 #define CANTP_USE_FUNCTIONAL_REQUEST_ADDRESSING    STD_OFF
 
 #if     (CANTP_USE_CAN_FD == STD_OFF)
-#define MAX_SEGMENT_DATA_SIZE       8
-#else   MAX_SEGMENT_DATA_SIZE      64
+#define MAX_SEGMENT_DATA_SIZE       8U
+#else   
+#define MAX_SEGMENT_DATA_SIZE       64U
 #endif
 
 #if     (CANTP_USE_EXTENDED_ADDRESSING == STD_OFF)   //The old were MAX_PAYLOAD_SF_STD_ADDR,MAX_PAYLOAD_SF_EXT_ADDR
-#define MAX_PAYLOAD_SF              7
-#define MAX_PAYLOAD_FF              6
-#define MAX_PAYLOAD_CF              7
+#define MAX_PAYLOAD_SF              7U
+#define MAX_PAYLOAD_FF              6U
+#define MAX_PAYLOAD_CF              7U
 #else
-#define MAX_PAYLOAD_SF              6
-#define MAX_PAYLOAD_FF              5
-#define MAX_PAYLOAD_CF              6
+#define MAX_PAYLOAD_SF              6U
+#define MAX_PAYLOAD_FF              5U
+#define MAX_PAYLOAD_CF              6U
 #endif
 
 uint8 MaxSegSize = MAX_SEGMENT_DATA_SIZE;
